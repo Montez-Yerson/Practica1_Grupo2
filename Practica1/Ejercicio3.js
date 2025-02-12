82a1784f30ff2228e2f0a35413c0633dd82f7654
Montez-Yerson <kowalskimontez@gmail.com>
2025-02-11 20:53:46 -0500

    Subiendo los ejercicios en comentario

---
+++ Practica1/Ejercicio3.js
@@ -0,0 +1,7 @@
+/* Los tramos del IVA en un país son los siguientes:
+
+Básico: 10%
+Reducido: 5%
+Superreducido: 2%
+Escribe un programa que pregunte al usuario el importe de una compra y el tipo de IVA aplicado 
+(básico, reducido, superreducido) y muestre por pantalla el total con IVA incluido. (10p) */
