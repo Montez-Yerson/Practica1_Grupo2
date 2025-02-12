8bcb3f4e1161a882b36f8aade55c93f6f7748086
Jeanlssj <jeancarlo.allicaco@gmail.com>
2025-02-12 18:12:58 -0500

    Subir ejercicio 1 por Jeancarlo

--- Practica1/Ejercicio1.js
+++ Practica1/Ejercicio1.js
@@ -5,3 +5,20 @@ Calcula el Índice de Masa Corporal (IMC) usando la fórmula IMC = peso / (altur
 muestra la categoría correspondiente según el IMC (bajo peso, normal, sobrepeso, obesidad). (10 p) */
 
 
+let peso = parseFloat(prompt("¿Cúanto es tu peso en kilogramos(kg)?: "));
+let altura = parseFloat(prompt("¿Cúanto es tu altura en metros(m)?: "));
+let imc = peso / (altura * altura);
+imc= parseFloat(imc.toFixed(2));
+
+if (imc < 18.5){
+    alert("Bajo peso con un IMC de: " + imc);
+}
+else if (imc >=18.5 && imc<=24.9){
+        alert("Estás en la categoría normal con un IMC de: " + imc);
+}
+else if (imc >=25 && imc < 29.9){
+        alert("Estás en la categoría sobrepeso con un IMC de: " + imc);
+}
+else{
+        alert("Estás en la categoría de obesidad con un IMC de: " + imc);
+}
