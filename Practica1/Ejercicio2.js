c5c3e30094718e8e1625d57578740e98b8985889
joelNV <joelnapan8@gmail.com>
2025-02-12 19:04:13 -0500

    ejercicio2

--- Practica1/Ejercicio2.js
+++ Practica1/Ejercicio2.js
@@ -4,8 +4,8 @@ debes tener al menos 18 años y tus ingresos mensuales deben estar entre 500€ 
 Escribe un programa que pregunte al usuario su edad y sus ingresos mensuales y 
 muestre por pantalla si el usuario es elegible para el subsidio o no. (10p) */
 
-let edad = parselnt(prompt("Introduce tu edad: "));
-let ingresos = parselnt(prompt("Introduce tus ingresos mensuales: "));
+let edad = parseInt(prompt("Introduce tu edad: "));
+let ingresos = parseInt(prompt("Introduce tus ingresos mensuales: "));
 if (edad >= 18 && ingresos >= 500 && ingresos <= 2000) {
     alert("Eres elegible para el subsidio");
 } else {
